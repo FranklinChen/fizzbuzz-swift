f69b6ec798766ed552689899d46c4bf98b1b37f0
Franklin Chen <franklinchen@franklinchen.com>
2014-06-06 08:40:32 -0400

    Initial Commit

---
+++ fizzbuzz-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fizzbuzz-swift
+//
+//  Created by Franklin Chen on 6/6/14.
+//  Copyright (c) 2014 Franklin Chen. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
